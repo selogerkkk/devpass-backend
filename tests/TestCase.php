b083f4123cbee7e90993002aa61ae7df7dc5fcb5
selogerkkk <ruanhborges@gmail.com>
2024-06-09 00:41:00 -0300

    feat: handles with auth

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $token = JWTAuth::fromUser($this->user);
+
+        $this->withHeader('Authorization', 'Bearer ' . $token);
+    }
 }
