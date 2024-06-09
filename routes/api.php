ecf3640e44cd216b0ed5c3ce4751a69f9118bbd0
selogerkkk <ruanhborges@gmail.com>
2024-06-09 14:33:12 -0300

    refactor: Update API routes structure and add middleware for CORS handling

--- routes/api.php
+++ routes/api.php
@@ -7,38 +7,40 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\ComunidadeController;
 
-Route::prefix('v1')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
+Route::group(['middleware' => 'cors'], function () {
+    Route::prefix('v1')->group(function () {
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [UserController::class, 'store'])->name('users.store');
 
-    Route::group(['middleware' => ['apiJwt']], function () {
-        Route::prefix('users')->name('.users')->group(function () {
-            Route::get('/', [UserController::class, 'index'])->name('.index');
-            Route::post('/', [UserController::class, 'store'])->name('.store');
-            Route::get('/{id}', [UserController::class, 'show'])->name('.show');
-            Route::delete('/{id}', [UserController::class, 'destroy'])->name('.destroy');
-            Route::post('/{id}/subscribe', [UserController::class, 'subscribeToCommunity'])->name('.subscribe');
-        });
+        Route::group(['middleware' => ['apiJwt']], function () {
+            Route::prefix('users')->name('.users')->group(function () {
+                Route::get('/', [UserController::class, 'index'])->name('.index');
+                Route::get('/{id}', [UserController::class, 'show'])->name('.show');
+                Route::delete('/{id}', [UserController::class, 'destroy'])->name('.destroy');
+                Route::post('/{id}/subscribe', [UserController::class, 'subscribeToCommunity'])->name('.subscribe');
+            });
 
-        Route::prefix('comunidades')->name('.comunidades')->group(function () {
-            Route::get('/', [ComunidadeController::class, 'index'])->name('.index');
-            Route::get('/{id}', [ComunidadeController::class, 'show'])->name('.show');
-            Route::post('/', [ComunidadeController::class, 'store'])->name('.store');
-            Route::delete('/{id}', [ComunidadeController::class, 'destroy'])->name('.destroy');
-        });
+            Route::prefix('comunidades')->name('.comunidades')->group(function () {
+                Route::get('/', [ComunidadeController::class, 'index'])->name('.index');
+                Route::get('/{id}', [ComunidadeController::class, 'show'])->name('.show');
+                Route::post('/', [ComunidadeController::class, 'store'])->name('.store');
+                Route::delete('/{id}', [ComunidadeController::class, 'destroy'])->name('.destroy');
+            });
 
-        Route::prefix('cursos')->name('.cursos')->group(function () {
-            Route::get('cursos', [CursoController::class, 'listarCursos'])->name('.index');
-            Route::post('/{cursoId}/inscrever', [CursoController::class, 'inscreverUsuario'])->name('.purchase');
-            Route::post('/', [CursoController::class, 'cadastrarCurso'])->name('.store');
-            Route::put('/{id}', [CursoController::class, 'atualizarCurso'])->name('.update');
-            Route::delete('/{id}', [CursoController::class, 'excluirCurso'])->name('.destroy');
-        });
+            Route::prefix('cursos')->name('.cursos')->group(function () {
+                Route::get('cursos', [CursoController::class, 'listarCursos'])->name('.index');
+                Route::post('/{cursoId}/inscrever', [CursoController::class, 'inscreverUsuario'])->name('.purchase');
+                Route::post('/', [CursoController::class, 'cadastrarCurso'])->name('.store');
+                Route::put('/{id}', [CursoController::class, 'atualizarCurso'])->name('.update');
+                Route::delete('/{id}', [CursoController::class, 'excluirCurso'])->name('.destroy');
+            });
 
-        Route::prefix('perfis')->name('.perfis')->group(function () {
-            Route::post('/', [PerfilController::class, 'store'])->name('.store');
-            Route::get('/{userId}', [PerfilController::class, 'show'])->name('.show');
-            Route::put('/{userId}', [PerfilController::class, 'update'])->name('.update');
-            Route::delete('/{userId}', [PerfilController::class, 'destroy'])->name('.destroy');
+            Route::prefix('perfis')->name('.perfis')->group(function () {
+                Route::post('/', [PerfilController::class, 'store'])->name('.store');
+                Route::get('/{userId}', [PerfilController::class, 'show'])->name('.show');
+                Route::put('/{userId}', [PerfilController::class, 'update'])->name('.update');
+                Route::delete('/{userId}', [PerfilController::class, 'destroy'])->name('.destroy');
+            });
         });
     });
 });
