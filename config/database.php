1e5d2399f6e9c235dc3ebcfbd53efbff25fd5b15
selogerkkk <ruanhborges@gmail.com>
2024-06-09 00:41:25 -0300

    fix: database errors

--- config/database.php
+++ config/database.php
@@ -37,10 +37,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'database' => dirname(__DIR__) . '/database/database.sqlite',
         ],
 
         'mysql' => [
@@ -125,7 +122,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
