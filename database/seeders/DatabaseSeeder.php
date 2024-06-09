b02087e2e583dee1170155cf2d47ba6a1690b77f
selogerkkk <ruanhborges@gmail.com>
2024-06-08 22:23:40 -0300

    feat: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,20 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Comunidade;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Comunidade::factory(5)->create()->each(function ($comunidade) use ($users) {
+            $comunidade->membros()->attach(
+                $users->random(rand(1, 5))->pluck('id')->toArray()
+            );
+        });
     }
 }
