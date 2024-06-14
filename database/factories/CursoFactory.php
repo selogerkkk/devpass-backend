425b91be9827c856a40433fe7ba00ff6270486d3
selogerkkk <ruanhborges@gmail.com>
2024-06-14 17:11:42 -0300

    feat: Update CursoFactory to generate random duration for courses

--- database/factories/CursoFactory.php
+++ database/factories/CursoFactory.php
@@ -16,7 +16,7 @@ class CursoFactory extends Factory
             'descricao' => $this->faker->paragraph,
             'professor' => $this->faker->name,
             'conteudo' => json_encode($this->faker->paragraphs(rand(2, 5))),
-            'duracao' => $this->faker->randomElement(['1 hora', '2 horas', '3 horas']),
+            'duracao' => $this->faker->time('H:i', 'now'),
             'preco' => $this->faker->randomFloat(2, 10, 100),
             'thumb' => $this->faker->imageUrl(640, 480, 'cursos', true),
             'rating' => $this->faker->numberBetween(1, 5),
